10f7fb058eff838c2b076e2f0596ff5689a59784
Yuka Masuda <yuka1110m@gmail.com>
2025-05-18 10:23:52 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { MobileWarningProvider } from './contexts/MobileWarningContext';
-import { PerformanceProvider } from './contexts/PerformanceContext';
 import { MenuBar } from './components/MenuBar';
 import { AboutPage } from './pages/AboutPage';
 import { FaqPage } from './pages/FaqPage';
 import { EmojiMaker } from './pages/EmojiMaker';
 import { ThemeToggle } from './components/ThemeToggle';
 import { MobileWarning } from './components/MobileWarning';
-import { PerformanceToggle } from './components/PerformanceToggle';
 import { Footer } from './components/Footer';
 import './styles/a11y.css';
 
@@ -19,31 +17,25 @@ function App() {
     <BrowserRouter>
       <MobileWarningProvider>
         <ThemeProvider>
-          <PerformanceProvider>
-            <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors flex flex-col">
-              <Toaster 
-                position="top-right"
-                expand={false}
-                richColors
-                closeButton
-              />
-              <MobileWarning />
-              <ThemeToggle />
-              {/* Position it below theme toggle with more space */}
-              <div style={{ position: 'fixed', top: '60px', right: '16px', zIndex: 50 }}>
-                <PerformanceToggle />
-              </div>
-              <MenuBar />
-              <main className="flex-grow">
-                <Routes>
-                  <Route path="/" element={<EmojiMaker />} />
-                  <Route path="/about" element={<AboutPage />} />
-                  <Route path="/faq" element={<FaqPage />} />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
-          </PerformanceProvider>
+          <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors flex flex-col">
+            <Toaster 
+              position="top-right"
+              expand={false}
+              richColors
+              closeButton
+            />
+            <MobileWarning />
+            <ThemeToggle />
+            <MenuBar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<EmojiMaker />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/faq" element={<FaqPage />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </MobileWarningProvider>
     </BrowserRouter>
