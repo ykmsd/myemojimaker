51c7bf999c5b4df27a3a6628f65b2e39ef969376
Yuka Masuda <yuka1110m@gmail.com>
2025-10-06 13:19:58 +0200

    Updated EmojiPanel.tsx

--- src/components/EmojiPanel.tsx
+++ src/components/EmojiPanel.tsx
@@ -58,14 +58,14 @@ const EmojiPanel: React.FC<EmojiPanelProps> = ({
 
         const gif = new GIF({
           workers: 4,
-          quality: 15,
+          quality: 25,
           repeat: 0,
           width: WIDTH,
           height: HEIGHT,
           transparent: 0x000000,
           background: null,
           workerScript: import.meta.env.PROD ? '/gif.worker.js' : '/public/gif.worker.js',
-          dither: false,
+          dither: true,
           debug: false
         });
 
